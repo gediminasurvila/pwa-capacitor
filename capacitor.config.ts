db41beea2d1859ed9dfd479c53754c987bd45d93
Gediminas Survila <survila.gediminas@gmail.com>
2025-10-06 15:18:14 +0300

    capacitor config fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.example.cappwatest",
   appName: "Capacitor PWA Test",
-  webDir: ".",
+  webDir: "dist",
   server: {
     // For iOS native build, point this to your deployed PWA URL
     url: "https://pwa-capacitor.netlify.app",
